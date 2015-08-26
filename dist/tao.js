469699c7c679317b5e6d34463d9fa328c46ac161
danyelangel <danielangel@Daniels-MacBook-Pro.local>
2015-08-26 19:54:43 -0400

    Initial commit

---
+++ dist/tao.js
@@ -0,0 +1,23 @@
+(function(root, undefined) {
+
+  "use strict";
+
+
+/* tao main */
+
+// Base function.
+var tao = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+tao.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.tao = tao;
+
+
+}(this));
---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* tao main */
+
+// Base function.
+var tao = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+tao.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.tao = tao;
